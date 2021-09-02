12482955b1912848d3d15598a722087b86ae58ae
Kyle Schrader <kyle.schrader97@gmail.com>
2021-09-02 17:00:44 -0500

    began exercise for syntax, types, and variables.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,20 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.print("Hello, World!");
-        System.out.print("Hello, World!");
+//        System.out.print("Hello, World!");
+//        System.out.print("Hello, World!");
+
+        int myFavoriteNumber = 88;
+
+//        System.out.print(myFavoriteNumber);
+
+        String myString = "Kyle is RAD";
+
+//        System.out.print(myString);
+
+        long myNumber = 123L;
+
+        System.out.print(myNumber);
     }
 
 }
