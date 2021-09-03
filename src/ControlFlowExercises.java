bcd0e0d83a92fb30a5a9c5975963962165a2c608
Kyle Schrader <kyle.schrader97@gmail.com>
2021-09-03 16:43:52 -0500

    finished the first few exercises on Control Flow and finished Control Mini Exercises

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,31 @@
+public class ControlFlowExercises {
+
+    public static void main(String[] args) {
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.print(i + " ");
+//            i++;
+//        }
+
+//        int i = 100;
+//        do {
+//            System.out.println(i);
+//            i -= 5;
+//        } while (i >= -10);
+
+        // Below will cause an infinite loop unless i is set to (i > 0) in the while. Why?
+        // Will infinite loop if while is set to any number higher than 65536.
+
+//        int i = 2;
+//        do {
+//            System.out.println(i);
+//            i *= i;
+//        } while (i > 0 && i < 1000000);
+
+        // Above code will be refactored to For loops below:
+
+
+
+    }
+
+}
---
+++ src/ControlMiniExercises.java
@@ -0,0 +1,30 @@
+public class ControlMiniExercises {
+
+    public static void main(String[] args) {
+//        int i = 5;
+//        int i = -5;
+//        if (i > 0) {
+//            System.out.println("Number is positive.");
+//        } else {
+//            System.out.println("Number is negative.");
+//        }
+    }
+
+}
+
+class ControlMiniExercisesTwo {
+
+    public static void main(String[] args) {
+//        int i = 51;
+//        int i = -51;
+//        int i = 10;
+//        i *= 2;
+//        if (i < -100) {
+//            System.out.println("That number is less than -100.");
+//        } else if (i > 100) {
+//            System.out.println("That number is more than 100.");
+//        } else {
+//            System.out.println("That number is between -100 and 100.");
+//        }
+    }
+}
