4e91181123acf7a52b7523ac76f60730979aa538
Kyle Schrader <kyle.schrader97@gmail.com>
2021-09-03 09:32:15 -0500

    commiting finished Syntax, Types, and Variables ex

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -12,9 +12,48 @@ public class HelloWorld {
 
 //        System.out.print(myString);
 
-        long myNumber = 123L;
+        float myNumber = 314;
 
-        System.out.print(myNumber);
+//        System.out.print(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+        // Above attempts to change format to object. Below skips that step.
+
+//        int three = (int) "three";
+
+//        int x = 4;
+////        x = x + 5;
+//        x += 5;
+//        System.out.print(x);
+
+//        int x = 3;
+//        int y = 4;
+////        y = y * x;
+//        y *= x;
+//        System.out.print(y);
+
+//        int x = 10;
+//        int y = 2;
+////        x = x / y;
+////        y = y - x;
+//        x /= y;
+//        y -= x;
+//        System.out.println(x);
+//        System.out.print(y);
+
+//        byte thisNumber = 127;
+////        byte thisNumber = 128;
+//        System.out.print(++thisNumber);
     }
-
 }
