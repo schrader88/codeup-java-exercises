b6d76171f27b93ee87a540cbebe39fa1d44d7deb
Kyle Schrader <kyle.schrader97@gmail.com>
2021-09-03 10:49:15 -0500

    created file for Console IO ex. tested placeholder print

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,13 @@
+public class ConsoleExercises {
+
+    public static void main(String[] args) {
+
+//        String greeting = "Salutations";
+//        String name = "Kyle";
+//        System.out.printf("Hello there, %s. Nice to see you.\n", name);
+//        System.out.printf("%s, %s.",greeting, name);
+
+
+    }
+
+}
