069636d63517cc0b0ff4d10eea919e32bfd98f1c
Kyle Schrader <kyle.schrader97@gmail.com>
2021-09-07 10:31:51 -0500

    finished writing FizzBuzz test.

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -36,6 +36,20 @@ public class ControlFlowExercises {
 //            System.out.println(i);
 //        }
 
+        // FizzBuzz Test
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 5 == 0 && i % 3 ==0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+
 
     }
 
