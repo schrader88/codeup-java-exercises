2dc58536f3398dc862a791eade06946e569deebc
Kyle Schrader <kyle.schrader97@gmail.com>
2021-09-07 09:42:05 -0500

    finished mini-problem/exercise for conditionals.

--- src/myIOExperiment.java
+++ src/myIOExperiment.java
@@ -12,3 +12,27 @@ public class myIOExperiment {
     }
 
 }
+
+class mySecondIOExperiment {
+
+    public static void main(String[] args) {
+
+        // write a Java program that asks the user for their age and then tells them if they are a teenager or not.
+        // bonus: if not, tell them whether they are younger or older than a teenager.
+
+        Scanner secondScan = new Scanner(System.in);
+
+        System.out.println("Enter your age: ");
+        int userAge = secondScan.nextInt();
+
+        if (userAge > 18) {
+            System.out.println("User is over 18 years old.");
+        } else if (userAge < 18 && userAge > 12) {
+            System.out.println("User is a teenager under 18 years old.");
+        } else {
+            System.out.println("User is below 13 years old.");
+        }
+
+    }
+
+}
