bd2c14feca7393dc359ef83d2e280297e270a4ae
Kyle Schrader <kyle.schrader97@gmail.com>
2021-09-08 09:48:52 -0500

    finished Bob exercise. will most likely refactor.

---
+++ src/Bob.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Bob {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What's up, I'm Bob. What do you need?");
+
+        String userInput = scanner.nextLine();
+
+        if (userInput.contains("?")) {
+            System.out.println("Sure.");
+        } else if (userInput.contains("!")) {
+            System.out.println("Whoa, chill out!");
+        } else if (userInput.equals("")) {
+            System.out.println("Fine. Be that way!");
+        } else {
+            System.out.println("Whatever.");
+        }
+    }
+
+}
