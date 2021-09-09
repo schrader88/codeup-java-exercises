c2d6cd5f094a5d61cbf20c7f8a5720555619323d
Kyle Schrader <kyle.schrader97@gmail.com>
2021-09-09 13:43:36 -0500

    finished MVP and bonuses for HighLow class

--- src/HighLow.java
+++ src/HighLow.java
@@ -10,11 +10,13 @@ public class HighLow {
 
         String userContinue = "";
 
+        int guessAccumulator = 0;
+
+        int randomNumber = (int) (Math.random() * 100) + 1;
+
         do {
 
-            int randomNumber = (int) (Math.random() * 100) + 1;
-
-//            System.out.println(randomNumber);
+            System.out.println(randomNumber);
 
             System.out.println("Guess the number between 1 and 100: ");
             int userGuess = scanner.nextInt();
@@ -24,13 +26,23 @@ public class HighLow {
             } else if (userGuess > randomNumber) {
                 System.out.println("LOWER");
             } else {
-                System.out.println("GOOD GUESS!");
+                System.out.println("GOOD GUESS! That is correct!");
+                break;
             }
+
+            if (guessAccumulator == 5) {
+                System.out.println("Shoot! That is 5 guesses. Better luck next time!");
+                break;
+            }
+
+            guessAccumulator++;
 
             System.out.println("Would you like to play again? (yes/no)");
             userContinue = scanner.next();
 
 
         } while (userContinue.equalsIgnoreCase("yes"));
+
+        System.out.println("You guessed a total of " + guessAccumulator + " times.");
     }
 }
