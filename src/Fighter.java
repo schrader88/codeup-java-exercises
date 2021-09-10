9ee1f67194aa4bd05ee4d451926f9666ae9aaba3
Kyle Schrader <kyle.schrader97@gmail.com>
2021-09-10 10:17:10 -0500

    created a Fighter class and FighterTest to test code

---
+++ src/Fighter.java
@@ -0,0 +1,26 @@
+public class Fighter {
+    // instance variables
+    public String name;
+    public int hitPoints;
+    public int maxDamage;
+
+    // methods
+    public void printStats(){
+        System.out.println(name + " has " + hitPoints + " hit points and can do " + maxDamage + " damage.");
+    }
+
+    public void battleRoar(){
+        System.out.println("I am " + name + ", prepare to be defeated!");
+    }
+
+    // DON'T DO THIS: PUT THE MAIN METHOD IN THE CLASS DEFINITION
+    // Instead, create a Test class rather than testing in main of the same class
+//    public static void main(String[] args) {
+//        Fighter arata = new Fighter();
+//        arata.name = "Arata";
+//        arata.hitPoints = 17;
+//        arata.maxDamage = 14;
+//        arata.printStats();
+//    }
+
+}
---
+++ src/FighterTest.java
@@ -0,0 +1,16 @@
+public class FighterTest {
+    public static void main(String[] args) {
+        // we instantiate a fighter object
+        Fighter arata = new Fighter();
+        arata.name = "Arata";
+        arata.hitPoints = 17;
+        arata.maxDamage = 14;
+        arata.printStats();
+        arata.battleRoar();
+
+        Fighter sozalix = new Fighter();
+        sozalix.name = "Soalix";
+        sozalix.battleRoar();
+        sozalix.printStats();
+    }
+}
