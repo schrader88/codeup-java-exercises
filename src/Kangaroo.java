8a24689f26f2abc069b312ecb55790cc1a457221
Kyle Schrader <kyle.schrader97@gmail.com>
2021-09-10 10:33:39 -0500

    created a Kangaroo class with a KangarooTest class. finished mini exercise.

---
+++ src/Kangaroo.java
@@ -0,0 +1,12 @@
+public class Kangaroo {
+    public String name;
+    public String sound;
+
+    public void makeNoise(){
+        System.out.println(name + " goes " + sound);
+    }
+
+    public void hippityHoppity(){
+        System.out.println(name + " jumps around the room! Vibin' and bouncin'!");
+    }
+}
---
+++ src/KangarooTest.java
@@ -0,0 +1,9 @@
+public class KangarooTest {
+    public static void main(String[] args) {
+        Kangaroo kanga = new Kangaroo();
+        kanga.name = "Kanga";
+        kanga.sound = "BOING";
+        kanga.makeNoise();
+        kanga.hippityHoppity();
+    }
+}
