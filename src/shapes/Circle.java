6f2395988fb14696ff6b7718ed5749570de51d24
Kyle Schrader <kyle.schrader97@gmail.com>
2021-09-10 16:19:41 -0500

    finished circle exercise.

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -1,13 +1,15 @@
 package shapes;
 
 public class Circle {
-    public Circle(double radius) {
+    private double radius;
 
+    public Circle(double radius) {
+        this.radius = radius;
     }
     public double getArea() {
-
+        return (Math.PI * (radius * radius));
     }
     public double getCircumference() {
-
+        return (2 * Math.PI * radius);
     }
 }
--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -2,5 +2,12 @@ package shapes;
 import util.Input;
 
 public class CircleApp {
+    public static void main(String[] args) {
+        Input input = new Input();
+        int radius = input.getInt();
+        Circle myCircle = new Circle(radius);
 
+        System.out.println("The area of the cirlce is: " + myCircle.getArea() + " ft.");
+        System.out.println("The circumference of the circle is: " + myCircle.getCircumference() + " ft.");
+    }
 }
