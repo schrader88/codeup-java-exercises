801b280ee0c33ef3221c8746d1262e322b23895b
Kyle Schrader <kyle.schrader97@gmail.com>
2021-09-13 09:23:30 -0500

    changed the methods to static methods, tested static methods, and added a method for user input

--- src/MyMathMethods.java
+++ src/MyMathMethods.java
@@ -1,11 +1,35 @@
+import java.util.Scanner;
+
 public class MyMathMethods {
 
-    public int multiply(int num1, int num2) {
+    // Both methods below are NOT STATIC:
+
+//    public int multiply(int num1, int num2) {
+//        return num1 * num2;
+//    }
+//
+//    public double multiply(double num1, double num2) {
+//        return num1 * num2;
+//    }
+
+    // Methods below are STATIC:
+
+    public static int multiply(int num1, int num2) {
         return num1 * num2;
     }
 
-    public double multiply(double num1, double num2) {
+    public static double multiply(double num1, double num2) {
         return num1 * num2;
+    }
+
+    public static int multiply() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter two integers: ");
+        int userFirst = scanner.nextInt();
+        int userSec = scanner.nextInt();
+
+        return userFirst * userSec;
     }
 
 }
--- src/MyMathMethodsTest.java
+++ src/MyMathMethodsTest.java
@@ -1,9 +1,25 @@
 public class MyMathMethodsTest {
     public static void main(String[] args) {
-        MyMathMethods myMath = new MyMathMethods();
 
-        System.out.println(myMath.multiply(2, 4));
-        System.out.println(myMath.multiply(2.5, 4.5));
+        // Below line is needed if the method is NOT STATIC:
+
+//        MyMathMethods myMath = new MyMathMethods();
+
+        // Before STATIC applied:
+
+//        System.out.println(myMath.multiply(2, 4));
+//        System.out.println(myMath.multiply(2.5, 4.5));
+
+        // After STATIC applied:
+
+//        System.out.println(MyMathMethods.multiply(2, 4));
+//        System.out.println(MyMathMethods.multiply(2.5, 4.5));
+
+        // User Input:
+
+        System.out.println(MyMathMethods.multiply());
+
+
 
 
     }
