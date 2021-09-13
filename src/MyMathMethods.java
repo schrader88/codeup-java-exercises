da4039de1b57e2551fd3d700ff75755ff37579a5
Kyle Schrader <kyle.schrader97@gmail.com>
2021-09-13 09:11:23 -0500

    finished MyMathMethods Warmup MVP and Bonus.

---
+++ src/MyMathMethods.java
@@ -0,0 +1,11 @@
+public class MyMathMethods {
+
+    public int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public double multiply(double num1, double num2) {
+        return num1 * num2;
+    }
+
+}
---
+++ src/MyMathMethodsTest.java
@@ -0,0 +1,10 @@
+public class MyMathMethodsTest {
+    public static void main(String[] args) {
+        MyMathMethods myMath = new MyMathMethods();
+
+        System.out.println(myMath.multiply(2, 4));
+        System.out.println(myMath.multiply(2.5, 4.5));
+
+
+    }
+}
