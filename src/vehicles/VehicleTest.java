28d9227603b560e4990d8b44b404aef6698d8bcb
Kyle Schrader <kyle.schrader97@gmail.com>
2021-09-15 11:36:23 -0500

    made inline notes on VehicleTest class to better understand functionality

--- src/vehicles/VehicleTest.java
+++ src/vehicles/VehicleTest.java
@@ -2,15 +2,24 @@ package vehicles;
 
 public class VehicleTest {
     public static void main(String[] args) {
+        // Below is with default constructor (or no arg constructor)
+
 //        Vehicle vehicle = new Vehicle();
 //        vehicle.setName("Generic cars.Vehicle");
 
+        // Below is with the constructor that accepts a string as an arg:
+
         Vehicle vehicle = new Vehicle("Generic Car");
+
         System.out.println(vehicle.getName() + " goes: ");
         vehicle.makeNoise();
 
+        // Below is with default constructor (or no arg constructor)
+
 //        FourWheeler fourWheeler = new FourWheeler();
 //        fourWheeler.setName("Four Wheeler");
+
+        // Below is with the constructor that accepts a string as an arg:
 
         FourWheeler fourWheeler = new FourWheeler("Four Wheeler");
 
@@ -20,9 +29,13 @@ public class VehicleTest {
         System.out.println(fourWheeler.getName() + " goes: ");
         fourWheeler.breakingDownNoise();
 
+        // Below is with the constructor that accepts a string as an arg:
+
         Car car = new Car("Honda Accord");
         System.out.println(car.getName() + " goes: ");
         car.makeNoise();
+
+        // Below is with the constructor that accepts a string as an arg:
 
         Motorcycle motorcycle = new Motorcycle("Harley");
         System.out.println(motorcycle.getName() + " goes: ");
