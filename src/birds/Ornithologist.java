a75e2e1fd7abf2f605569ab5a70f743c39c25778
Kyle Schrader <kyle.schrader97@gmail.com>
2021-09-16 09:39:29 -0500

    refactored changeBirdName method. will now store oldBirdName in a String variable.

--- src/birds/Ornithologist.java
+++ src/birds/Ornithologist.java
@@ -2,7 +2,8 @@ package birds;
 
 public class Ornithologist {
     public void changeBirdName(String newBirdName, Bird birdToRename) {
-        System.out.println(birdToRename.getName() + "'s name has been changed to " + newBirdName + ".");
+        String oldBirdName = birdToRename.getName();
         birdToRename.setName(newBirdName);
+        System.out.println(oldBirdName + "'s name has been changed to " + newBirdName + ".");
     }
 }
