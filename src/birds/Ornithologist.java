1f081f9683fa87f4074819c7055c7688e1556bfc
Kyle Schrader <kyle.schrader97@gmail.com>
2021-09-16 09:42:03 -0500

    changed the reference in the concat to getName instead of printing the entered string. this ensures that the name is in fact changing.

--- src/birds/Ornithologist.java
+++ src/birds/Ornithologist.java
@@ -4,6 +4,6 @@ public class Ornithologist {
     public void changeBirdName(String newBirdName, Bird birdToRename) {
         String oldBirdName = birdToRename.getName();
         birdToRename.setName(newBirdName);
-        System.out.println(oldBirdName + "'s name has been changed to " + newBirdName + ".");
+        System.out.println(oldBirdName + "'s name has been changed to " + birdToRename.getName() + ".");
     }
 }
