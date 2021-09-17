aa156dda9cb3c173165dfc7f7a7ffb12159f0531
Kyle Schrader <kyle.schrader97@gmail.com>
2021-09-17 14:59:01 -0500

    finished the exercise More Shapes exercises

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,18 +1,21 @@
 package shapes;
 
 public class Square extends Quadrilateral{
+    protected double side;
+
     public Square(double side) {
         super(side, side);
+        this.side = side;
     }
 
     @Override
     public double getPerimeter() {
-        return 0;
+        return 4 * side;
     }
 
     @Override
     public double getArea() {
-        return 0;
+        return side * side;
     }
 
     @Override
