15bab2e46b5a937966a2ff8837f5a6de1cc8599d
Kyle Schrader <kyle.schrader97@gmail.com>
2021-09-21 12:29:42 -0500

    added two methods. one with a try-catch-finally and the other with multiple catches and a finally.

--- src/ExceptionsLecture/ExceptionsLecture.java
+++ src/ExceptionsLecture/ExceptionsLecture.java
@@ -1,8 +1,47 @@
 package ExceptionsLecture;
 
+import java.util.Scanner;
+
 public class ExceptionsLecture {
 
     public String couldBeAnInteger = "42";
     public String certainlyNotAnInteger = "No integer at all here";
+
+    public void askForAnInteger() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please enter an integer: ");
+        String userInput = sc.nextLine();
+        try {
+            int userInputInteger = Integer.parseInt(userInput);
+            System.out.println("The integer in your string is " + userInputInteger);
+        } catch(NumberFormatException nfx) {
+            System.out.println("Uh oh, " + nfx.getMessage() + " there is no integer!");
+        } finally {
+            System.out.println("Thank you for using this complex and useful app.");
+        }
+
+    }
+
+    public void aMoreComplexAskForInteger(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter an integer");
+        String userInput = scanner.nextLine();
+        try {
+            //userInput = null;
+            int userInputAsInteger = Integer.parseInt(userInput, 10, 15, 10);
+        } catch (NullPointerException npx){
+            System.out.println("Oops you didn't enter anything");
+            npx.printStackTrace();
+        } catch (IndexOutOfBoundsException iobx) {
+            System.out.println("Oops your string wasn't long enough");
+            iobx.printStackTrace();
+        } catch (NumberFormatException nfx){
+            System.out.println("No integer between chars 10 and 15");
+            nfx.printStackTrace();
+        } finally {
+            System.out.println("That was useful wasn't it?");
+        }
+    }
 
 }
--- src/ExceptionsLecture/ExceptionsLectureTest.java
+++ src/ExceptionsLecture/ExceptionsLectureTest.java
@@ -2,14 +2,19 @@ package ExceptionsLecture;
 
 public class ExceptionsLectureTest {
     public static void main(String[] args) {
-        try {
-            ExceptionsLecture el = new ExceptionsLecture();
-            System.out.println(Integer.parseInt(el.couldBeAnInteger));
-            System.out.println(Integer.parseInt(el.certainlyNotAnInteger));
-        } catch (NumberFormatException nfx) {
-            System.out.println("Got an exception: " + nfx);
-            System.out.println(nfx.getMessage());
-            nfx.printStackTrace();
-        }
+//        try {
+//            ExceptionsLecture el = new ExceptionsLecture();
+//            System.out.println(Integer.parseInt(el.couldBeAnInteger));
+//            System.out.println(Integer.parseInt(el.certainlyNotAnInteger));
+//        } catch (NumberFormatException nfx) {
+//            System.out.println("Got an exception: " + nfx);
+//            System.out.println(nfx.getMessage());
+//            nfx.printStackTrace();
+//        }
+
+        ExceptionsLecture el = new ExceptionsLecture();
+
+        el.askForAnInteger();
+        el.aMoreComplexAskForInteger();
     }
 }
