336b65c2d8a534100f2cbb3e0923e0419afb1cba
Kyle Schrader <kyle.schrader97@gmail.com>
2021-09-23 09:49:34 -0500

    finished all necessary testing in MovieWarmupTest class. finished with warmup

--- src/movieWarmup/MovieWarmupTest.java
+++ src/movieWarmup/MovieWarmupTest.java
@@ -1,9 +1,15 @@
 package movieWarmup;
 
+import movies.Movie;
+
 public class MovieWarmupTest {
     public static void main(String[] args) {
+        MovieWarmup mw = new MovieWarmup("The Wolf of Wall Street", "Martin Scorsese");
         Comedy comedy = new Comedy("Test Name", "Test Director", true);
 
+        System.out.println(mw.getDirector());
+        System.out.println(mw.getName());
 
+        System.out.println(comedy.isFunny());
     }
 }
