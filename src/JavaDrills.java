2fd5aa4beff6049b4c886c9605eef2c5615c254b
Kyle Schrader <kyle.schrader97@gmail.com>
2021-10-14 09:26:31 -0500

    first commit. not finished, work in progress

---
+++ src/JavaDrills.java
@@ -0,0 +1,23 @@
+//================================= WARM UP
+//        In `codeup-java-exercises`, create a class `JavaDrills`, with the following:
+//        Create a public static method, `flipOuterCase`, that takes in a string and returns the string
+//        with the case reversed for the first and last letters. Assume the input will be three or more characters long.
+//        Example inputs and expected outputs...
+//        System.out.println(flipOuterCase(“cat”)); // CaT
+//        System.out.println(flipOuterCase(“CaT”)); // cat
+//        System.out.println(flipOuterCase(“cAt”)); // CAT
+//        System.out.println(flipOuterCase(“caT”)); // Cat
+
+// Work in progress
+
+public class JavaDrills {
+    public static String flipOuterCase(String str) {
+        int length = str.length();
+        char first = str.charAt(0);
+        char last = str.charAt(length - 1);
+
+
+
+        return "";
+    }
+}
